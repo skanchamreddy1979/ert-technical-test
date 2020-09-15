35cfbba3d6b336440028b23a6ad1821a8011661c
Mark <mark.campbell@ert.com>
2020-09-15 14:02:37 +0100

    Base project setup
    - Starting point for codebase

---
+++ Controllers/BeerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ert_beer_app.Controllers
+{
+    [Route("api/beer")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+    }
+}
