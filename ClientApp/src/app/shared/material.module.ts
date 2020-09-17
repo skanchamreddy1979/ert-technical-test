8bd6456d01f96dd86d6d9e1c8712725ed41453ad
Suresh Kanchamreddy <suresh_kanchamreddy@epam.com>
2020-09-17 12:52:04 +0530

    intial chekin

---
+++ ClientApp/src/app/shared/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+  ],
+})
+export class MaterialModule {}
---
+++ README.md
@@ -0,0 +1,1 @@
+ert-technical-test
