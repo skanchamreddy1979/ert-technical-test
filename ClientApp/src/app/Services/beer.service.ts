71c5e6e5e8f120dca99ac3ad93696e89ecb124da
Srikanth Puli <25710514+srikanth8055@users.noreply.github.com>
2020-09-17 19:35:51 +0530

    edit service

--- ClientApp/src/app/Services/beer.service.ts
+++ ClientApp/src/app/Services/beer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Beer } from '../Models/beer';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +12,19 @@ export class BeerService {
   constructor(private http: HttpClient) { }
 
   getBeersWithPagination(page: number, perPage: number) {
-    return this.http.get('https://api.punkapi.com/v2/beers', { params: { page: `${page}`, per_page: `${perPage}` } });
+    return this.http.get(`${environment.apiUrl}beers`, { params: { page: `${page}`, per_page: `${perPage}` } });
   }
 
   getBeersWithFilter(brewedBefore: Date, abvGt: number) {
-    return this.http.get('https://api.punkapi.com/v2/beers', { params: { brewed_before: `${brewedBefore}`, abv_gt: `${abvGt}` } });
+    return this.http.get(`${environment.apiUrl}beers`, { params: { brewed_before: `${brewedBefore}`, abv_gt: `${abvGt}` } });
   }
 
   getBeerById(id: string) {
-    return this.http.get( `https://api.punkapi.com/v2/beers/${id}`);
+    return this.http.get( `${environment.apiUrl}beers/${id}`);
   }
 
   getBeer() {
-   return this.http.get('https://api.punkapi.com/v2/beers');
+   return this.http.get(`${environment.apiUrl}beers`);
   }
 
 }
