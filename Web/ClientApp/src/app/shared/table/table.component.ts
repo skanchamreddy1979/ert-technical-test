a32bca458682da48838246159854dbc283737468
Yauheni Aliakseyenka <yauheni_aliakseyenka@epam.com>
2020-09-23 16:49:40 +0300

    [Feature 4] implemented expanding of row with beer's details

--- Web/ClientApp/src/app/shared/table/table.component.ts
+++ Web/ClientApp/src/app/shared/table/table.component.ts
@@ -1,12 +1,20 @@
 import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger } from '@angular/animations';
+import {
   Component,
+  ContentChild,
   Input,
   OnChanges,
   OnInit,
   SimpleChanges,
+  TemplateRef,
   ViewChild,
   ViewEncapsulation } from '@angular/core';
-  
+
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -16,6 +24,13 @@ import { TableColumn } from 'src/app/core/models/table-column.model';
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
+  ],
   encapsulation: ViewEncapsulation.None
 })
 export class TableComponent implements OnInit, OnChanges  {
@@ -23,8 +38,10 @@ export class TableComponent implements OnInit, OnChanges  {
   @Input() columns: TableColumn[];
   @Input() data: any[];
   
+  @ContentChild('expandedRow', { static: false }) expandedRowTemplate: TemplateRef<any>;
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
+  expandedRow: any | null;
   pageSizeOptions: number[] = [10, 20];
   displayedColumns: string[];
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
@@ -47,6 +64,12 @@ export class TableComponent implements OnInit, OnChanges  {
   }
 
   ngOnInit(): void {
+  }
+
+  onRowClick(row: any) {
+    if (this.expandedRowTemplate) {
+      this.expandedRow = this.expandedRow == row ? null : row
+    }
   }
 
 }
