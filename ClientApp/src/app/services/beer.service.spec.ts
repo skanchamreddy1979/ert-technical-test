e9e9a5baee1822f3dd6eae8e8cdea33130e1ff84
Vasili Holub <vasili_holub@epam.com>
2020-09-28 00:57:19 +0300

    added test for service

---
+++ ClientApp/src/app/services/beer.service.spec.ts
@@ -0,0 +1,42 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { IBeer } from '../models/beer';
+import { TestData } from '../test-data';
+
+import { BeerService } from './beer.service';
+
+describe('BeerService', () => {
+    let service: BeerService;
+    let httpmock: HttpTestingController;
+    const mockResponse: IBeer[] = TestData.getBeerTestData();
+
+    beforeEach(() => TestBed.configureTestingModule(
+      {
+        imports: [
+          HttpClientTestingModule
+        ],
+        providers: [BeerService]
+      }
+    ));
+
+    beforeEach(() => {
+      service = TestBed.inject(BeerService);
+      httpmock = TestBed.inject(HttpTestingController);
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('expects service to retrieve all beers', () => {
+      service.getBeers().subscribe(data => {
+        expect(mockResponse.length).toEqual(1);
+      });
+    });
+
+  it('expects service to retrieve beer by id ', () => {
+      service.getBeer(2).subscribe(data => {
+        expect(mockResponse.length).toEqual(1);
+      });
+    });
+});
