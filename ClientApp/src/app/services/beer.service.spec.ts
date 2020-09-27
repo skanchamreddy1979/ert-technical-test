54b992b47d791168daa0a6d7007087ec3e237d88
Vasili Holub <vasili_holub@epam.com>
2020-09-28 01:01:46 +0300

    updated tests

--- ClientApp/src/app/services/beer.service.spec.ts
+++ ClientApp/src/app/services/beer.service.spec.ts
@@ -6,37 +6,37 @@ import { TestData } from '../test-data';
 import { BeerService } from './beer.service';
 
 describe('BeerService', () => {
-    let service: BeerService;
-    let httpmock: HttpTestingController;
-    const mockResponse: IBeer[] = TestData.getBeerTestData();
+  let service: BeerService;
+  let httpmock: HttpTestingController;
+  const mockResponse: IBeer[] = TestData.getBeerTestData();
 
-    beforeEach(() => TestBed.configureTestingModule(
-      {
-        imports: [
-          HttpClientTestingModule
-        ],
-        providers: [BeerService]
-      }
-    ));
+  beforeEach(() => TestBed.configureTestingModule(
+    {
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [BeerService]
+    }
+  ));
 
-    beforeEach(() => {
-      service = TestBed.inject(BeerService);
-      httpmock = TestBed.inject(HttpTestingController);
+  beforeEach(() => {
+    service = TestBed.inject(BeerService);
+    httpmock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('expects service to retrieve all beers', () => {
+    service.getBeers().subscribe(data => {
+      expect(mockResponse.length).toEqual(1);
     });
+  });
 
-    it('should be created', () => {
-      expect(service).toBeTruthy();
+it('expects service to retrieve beer by id ', () => {
+    service.getBeer(2).subscribe(data => {
+      expect(mockResponse.length).toEqual(1);
     });
-
-    it('expects service to retrieve all beers', () => {
-      service.getBeers().subscribe(data => {
-        expect(mockResponse.length).toEqual(1);
-      });
-    });
-
-  it('expects service to retrieve beer by id ', () => {
-      service.getBeer(2).subscribe(data => {
-        expect(mockResponse.length).toEqual(1);
-      });
-    });
+  });
 });
