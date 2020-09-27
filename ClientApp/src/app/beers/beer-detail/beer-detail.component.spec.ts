e2ee6eeec770426ce49d2328ea5c78d273e36150
Vasili Holub <vasili_holub@epam.com>
2020-09-28 00:57:56 +0300

    added test

--- ClientApp/src/app/beers/beer-detail/beer-detail.component.spec.ts
+++ ClientApp/src/app/beers/beer-detail/beer-detail.component.spec.ts
@@ -1,25 +1,55 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BeerDetailComponent } from './beer-detail.component';
+import { BeerService } from '../../services/beer.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { IBeer } from '../../models/beer';
+import { of } from 'rxjs';
+import { TestData } from '../../test-data';
 
 describe('BeerDetailComponent', () => {
   let component: BeerDetailComponent;
   let fixture: ComponentFixture<BeerDetailComponent>;
-
+  let beerService: BeerService;
+  const mockResponse: IBeer[] = TestData.getBeerTestData();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BeerDetailComponent ]
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterModule.forRoot([])
+      ],
+      declarations: [BeerDetailComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BeerDetailComponent);
     component = fixture.componentInstance;
+    beerService = TestBed.get(BeerService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call ngOnInit method', async(() => {
+
+    spyOn(beerService, 'getBeer').and.returnValue(of(mockResponse[0]));
+    component.ngOnInit();
+    component.beer = mockResponse[0];
+    expect(mockResponse[0]).toEqual(mockResponse[0]);
+  }));
+
+  it('should call ngOnInit method empty beer', async(() => {
+    let mockResponse: IBeer[] = [];
+    spyOn(beerService, 'getBeer').and.returnValue(of(mockResponse[0]));
+    component.ngOnInit();
+    component.beer = mockResponse[0];
+    expect(mockResponse[0]).toEqual(mockResponse[0]);
+  }));
+
 });
