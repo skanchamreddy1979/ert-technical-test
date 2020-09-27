5c2e2cbc6d6d8937cc9fc8e7d30948a492ea77c9
Vasili Holub <vasili_holub@epam.com>
2020-09-28 01:00:35 +0300

    added test

--- ClientApp/src/app/app.component.spec.ts
+++ ClientApp/src/app/app.component.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [
         AppComponent
       ],
@@ -12,20 +18,14 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular: Getting Started'`, () => {
+  it(`should have as title 'BrewDog Beers Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular: Getting Started');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('BrewDog Beers Management');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular: Getting Started!!');
-  });
 });
