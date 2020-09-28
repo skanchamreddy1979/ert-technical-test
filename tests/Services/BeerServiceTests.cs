dd8e56f3ac9e95e7f87112fb6cf05504c631ad9f
Aliaksandr Kamarou <aliaksandr_kamarou1@epam.com>
2020-09-28 19:07:13 +0300

    Add unit test

---
+++ tests/Services/BeerServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ert_beer_app.Commands;
+using ert_beer_app.DataAccess;
+using ert_beer_app.Models;
+using ert_beer_app.Services;
+using ert_beer_app.Services.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace tests.Services
+{
+    public class BeerServiceTests : IClassFixture<TestFixture>
+    {
+        private readonly BeerContext _beerContext;
+        private readonly IBeerService _beerService;
+
+        public BeerServiceTests(TestFixture testFixture)
+        {
+            _beerContext = testFixture.BeerContext;
+            _beerService = new BeerService(_beerContext);
+        }
+
+        [Fact]
+        public async Task SaveFavouritesForUser_WhenCorrectCommand_ShouldSaveFavourites()
+        {
+            var command = new SaveFavouriteBeersCommand
+                          {
+                              UserEmail = "test@email.com",
+                              ListBeer = new List<Beer> { TestBeer }
+                          };
+
+            await _beerService.SaveFavouriteBeersForUserAsync(command);
+
+            User user = await _beerContext.Users
+                                          .Include(c => c.Favourites)
+                                          .ThenInclude(f => f.Beer)
+                                          .FirstOrDefaultAsync(f => f.UserEmail.Equals(command.UserEmail));
+
+            Assert.Equal(command.ListBeer.Count(), user.Favourites.Count());
+            Assert.Contains(user.Favourites, f => f.Beer.BeerId == TestBeer.BeerId);
+        }
+
+        private Beer TestBeer => new Beer {BeerId = 1, Name = "test beer", Abv = 2.3, Description = "Test descr"};
+    }
+}
---
+++ tests/TestFixture.cs
@@ -0,0 +1,20 @@
+﻿using ert_beer_app.DataAccess;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace tests
+{
+    public class TestFixture
+    {
+        private BeerContext _beerContext;
+
+        public BeerContext BeerContext => _beerContext ??= GetBeerContext();
+
+        private BeerContext GetBeerContext()
+        {
+            return new BeerContext(new DbContextOptionsBuilder<BeerContext>()
+                                       .UseInMemoryDatabase(databaseName: "Beer")
+                                   .Options);
+        }
+    }
+}
