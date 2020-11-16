0293b7e70e77f382b6a2677149abde758ef0a5b4
Ilya Kaliaskin <ilya_kaliaskin@epam.com>
2020-11-16 17:05:41 +0300

    Add test for BeerMapper service

--- ClientApp/src/app/shared/punk-api/beer-dto-mapper.service.spec.ts
+++ ClientApp/src/app/shared/punk-api/beer-dto-mapper.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BeerDtoMapperService } from './beer-dto-mapper.service';
+import { BeerDto } from './beer-dto.model';
 
 describe('BeerDtoMapperService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,28 @@ describe('BeerDtoMapperService', () => {
   it('should be created', () => {
     const service: BeerDtoMapperService = TestBed.get(BeerDtoMapperService);
     expect(service).toBeTruthy();
+  });
+
+  it('should create Beer instance when BeerDto is provided', () => {
+    const beerDto = {
+      id: 1,
+      name: 'BeerName',
+      tagline: 'BeerTagLine',
+      first_brewed: 'BeerFirstBrewed',
+      description: 'BeerDescription',
+      image_url: 'BeerImageUrl',
+      abv: 5.0,
+    } as BeerDto;
+
+    const service: BeerDtoMapperService = TestBed.get(BeerDtoMapperService);
+
+    const beer = service.mapBeerDto(beerDto);
+    expect(beer.id).toBe(1);
+    expect(beer.name).toBe('BeerName');
+    expect(beer.tagLine).toBe('BeerTagLine');
+    expect(beer.firstBrewed).toBe('BeerFirstBrewed');
+    expect(beer.description).toBe('BeerDescription');
+    expect(beer.imageUrl).toBe('BeerImageUrl');
+    expect(beer.abv).toBe(5.0);
   });
 });
