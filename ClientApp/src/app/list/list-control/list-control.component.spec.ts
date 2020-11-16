02ec85492c8f9adb6cba076a649f8578bc8a0585
Ilya Kaliaskin <ilya_kaliaskin@epam.com>
2020-11-16 15:36:43 +0300

    Add tests for ListControl component

--- ClientApp/src/app/list/list-control/list-control.component.spec.ts
+++ ClientApp/src/app/list/list-control/list-control.component.spec.ts
@@ -26,8 +26,6 @@ describe('ListControlComponent', () => {
       ]
     })
     .compileComponents();
-
-    beerServiceSpy = TestBed.get(BeerService) as jasmine.SpyObj<BeerService>;
   }));
 
   beforeEach(() => {
@@ -38,6 +36,11 @@ describe('ListControlComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger beers load with initial pagination parameters on initialization', () => {
+    component.ngOnInit();
+    expect(beerServiceSpy.loadBeers).toHaveBeenCalledWith(1, 10);
   });
 
   it('should detect the last page if number of beers is less than page size', fakeAsync(() => {
@@ -52,4 +55,62 @@ describe('ListControlComponent', () => {
     tick();
     expect(component.reachedLastPage).toBe(false);
   }));
+
+  it('should reset pagination parameters to initial values on search submit', () => {
+    component.page = 3;
+    component.perPage = 15;
+    component.onSearchSubmit();
+    expect(component.page).toBe(1);
+    expect(component.perPage).toBe(10);
+  });
+
+  it('should load beers with initial pagination parameters and search value on search submit', () => {
+    component.page = 3;
+    component.perPage = 15;
+    component.searchValue = 'Lager';
+    component.onSearchSubmit();
+    expect(beerServiceSpy.loadBeers).toHaveBeenCalledWith(1, 10, 'Lager');
+  });
+
+  it('should reset page to initial value on changing number of items per page', () => {
+    component.page = 3;
+    component.onPerPageChange();
+    expect(component.page).toBe(1);
+  });
+
+  it('should load the first page of beers with specified per page number and search value on per page change', () => {
+    component.page = 3;
+    component.perPage = 15;
+    component.searchValue = 'Lager';
+    component.onPerPageChange();
+    expect(beerServiceSpy.loadBeers).toHaveBeenCalledWith(1, 15, 'Lager');
+  });
+
+  it('should decrement current page on previous page click', () => {
+    component.page = 3;
+    component.onPreviousPageClick();
+    expect(component.page).toBe(2);
+  });
+
+  it('should load the previous page of beers with specified per page number and search value on previous page click', () => {
+    component.page = 3;
+    component.perPage = 15;
+    component.searchValue = 'Lager';
+    component.onPreviousPageClick();
+    expect(beerServiceSpy.loadBeers).toHaveBeenCalledWith(2, 15, 'Lager');
+  });
+
+  it('should increment current page on next page click', () => {
+    component.page = 3;
+    component.onNextPageClick();
+    expect(component.page).toBe(4);
+  });
+
+  it('should load the previous page of beers with specified per page number and search value on previous page click', () => {
+    component.page = 3;
+    component.perPage = 15;
+    component.searchValue = 'Lager';
+    component.onNextPageClick();
+    expect(beerServiceSpy.loadBeers).toHaveBeenCalledWith(4, 15, 'Lager');
+  });
 });
