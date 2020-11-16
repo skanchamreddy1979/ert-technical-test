216472757a3e7c4cbe5d3075fc3ba0cf2f5789ad
Alexander Liakhovets <alexanderliakhovets@gmail.com>
2020-11-17 00:01:20 +0300

    fix style

--- ClientApp/src/modules/beer/beer.module.ts
+++ ClientApp/src/modules/beer/beer.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule } from "@angular/material/grid-list";
-import { HttpClientModule } from "@angular/common/http";
-import { MatCardModule } from "@angular/material/card";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { RouterModule } from "@angular/router";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { RouterModule } from '@angular/router';
 
 import { BeerListComponent } from './componets/beer-list/beer-list.component';
 import { BeerMainPageComponent } from './pages/beer-main-page/beer-main-page.component';
-import { MaterialModule } from "../material/material.module";
+import { MaterialModule } from '../material/material.module';
 import { BeerPreviewComponent } from './componets/beer-preview/beer-preview.component';
 import { BeerCardComponent } from './componets/beer-card/beer-card.component';
 import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
