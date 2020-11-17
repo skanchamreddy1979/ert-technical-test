aea9eee3f9bdcf7c80eed23d9d66e20aa27a5274
Yauhen_Bialiauski <belya0695@mail.ru>
2020-11-17 16:45:57 +0300

    Added way to unsubscribe

--- ClientApp/src/app/components/list/list.component.ts
+++ ClientApp/src/app/components/list/list.component.ts
@@ -1,7 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { Beer } from 'src/app/models/beer.model';
 import { BrewDogBeerService } from 'src/app/services/brewDogBeers.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -9,13 +11,14 @@ import { BrewDogBeerService } from 'src/app/services/brewDogBeers.service';
   styleUrls: ['./list.component.css'],
   providers: [BrewDogBeerService],
 })
-export class ListComponent implements OnInit, AfterViewInit {
+export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   displayedColumns: string[] = ['name', 'tagline', 'firstbrewed', 'abv'];
   beers: Beer[];
   beersTotalLength = 999; // couldn't find a way to get the total count of beer
   pageSize = 10;
   startPageIndex = 1;
+  notifier = new Subject();
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
@@ -23,15 +26,28 @@ export class ListComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.loadBeers(this.pageSize, this.startPageIndex);
-    
+
   }
 
   ngAfterViewInit() {
-    this.paginator.page.subscribe(() => this.loadBeers(this.paginator.pageSize, this.paginator.pageIndex + 1))
+    this.paginator
+      .page.pipe(takeUntil(this.notifier))
+      .subscribe(
+        () => this.loadBeers(this.paginator.pageSize, this.paginator.pageIndex + 1)
+      )
+  }
+
+  ngOnDestroy() {
+
   }
 
   private loadBeers(pageSize: number, pageNumber: number, searchByNameString?: string) {
-    this.beerService.getBeers(pageSize, pageNumber, searchByNameString).subscribe((data: Beer[]) => this.beers = data )
+    this.beerService
+      .getBeers(pageSize, pageNumber, searchByNameString)
+      .pipe(takeUntil(this.notifier))
+      .subscribe(
+        (data: Beer[]) => this.beers = data
+      )
   }
 
   onSearch(searchString) {
