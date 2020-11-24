d5f85719e3bc8ce8fb1d4523f3ad32a82a266fe4
Shubham Shekhar <Shubham_Shekhar@epam.com>
2020-11-24 21:42:38 +0530

    Add project files.

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+
+import { ListComponent } from './list/list.component';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, ListComponent, WelcomeComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: '', component: WelcomeComponent, pathMatch: 'full' },
+      { path: 'list', component: ListComponent },
+    ]),
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
