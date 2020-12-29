dfe095f6382cb28e419fffd8822e236ac0590831
Lakshminarayana Tanniru <Lakshminarayana_Tanniru@epam.com>
2020-12-30 00:24:15 +0530

    Pagination

--- ert-beer-app/Models/PaginatedList.cs
+++ ert-beer-app/Models/PaginatedList.cs
@@ -9,7 +9,7 @@ namespace ERT.Entities
     public class PaginatedList<T> : List<T>
     {
         public int PageIndex { get; private set; }
-        public int TotalPages { get; private set; }
+        public int LastPageIndex { get; private set; }
         [BindProperty(SupportsGet = true)]
         public int PageSize { get; set; } = 10;
 
@@ -17,12 +17,15 @@ namespace ERT.Entities
         public int CurrentPage { get; set; } = 1;
         public bool ShowFirst => CurrentPage != 1 ;
         public bool ShowPrevious => CurrentPage > 1 ;
-        public bool ShowNext => CurrentPage < TotalPages && PageSize >= 10 ;
-        public bool ShowLast => CurrentPage != TotalPages && PageSize >= 10 ;
+        public bool ShowNext => CurrentPage < LastPageIndex && PageSize >= 10 ;
+        public bool ShowLast => CurrentPage != LastPageIndex && PageSize >= 10 ;
         public PaginatedList(List<T> items, int count, int pageIndex, int lastPageIndex)
-        {            
-            CurrentPage = pageIndex; PageSize = count;
-            TotalPages = lastPageIndex;           
+        {   if(items.ToList().Count>0)
+            {
+                CurrentPage = pageIndex; PageSize = count;
+            }
+            
+            LastPageIndex = lastPageIndex;           
             this.AddRange(items);
         } 
 
